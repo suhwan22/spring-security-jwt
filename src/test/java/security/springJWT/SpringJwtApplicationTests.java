99fbfd78be3a4e3d785c922248c99f709897012d
suhwan <tnghkswkd123@naver.com>
2024-06-07 18:16:01 +0900

    chore: spring-security-jwt 진행을 위한 의존성 및 init

---
+++ src/test/java/security/springJWT/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package security.springJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

9bd21b21506c1adf81366ca524ac1d06d5889e12
suhwan <tnghkswkd123@naver.com>
2024-06-14 01:58:32 +0900

    fix: secret key 전달 오류 해결

--- src/main/java/security/springJWT/jwt/JWTUtil.java
+++ src/main/java/security/springJWT/jwt/JWTUtil.java
@@ -13,7 +13,7 @@ import java.util.Date;
 public class JWTUtil {
 	private final SecretKey secretKey;
 
-	public JWTUtil(@Value("$(spring.jwt.secret")String secret) {
+	public JWTUtil(@Value("${spring.jwt.secret}")String secret) {
 		this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
 	}
 
